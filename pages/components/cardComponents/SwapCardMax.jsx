5b5de5e41f33390fbf199ae7a20144cbb802af86
gitmazter <philip.andelic@gmail.com>
2023-09-05 20:06:14 +0700

    add half and 25%, improved state changes and style

--- pages/components/cardComponents/SwapCardMax.jsx
+++ pages/components/cardComponents/SwapCardMax.jsx
@@ -7,6 +7,7 @@ export const SwapCardMax = ({max, fromTo, currencies}) => {
   const { maxAmount, setMaxAmount } = max;
   const { balance } = useBalance()
   const { saveAmount } = useAmount()
+
   //console.log(fromTo);
   /* set amount to max value possible*/
   const setAmountToMax = () => {
@@ -14,49 +15,64 @@ export const SwapCardMax = ({max, fromTo, currencies}) => {
     document.getElementById('swapInput').value = maxAmount[`${fromTo.from.name}`]
     saveAmount(maxAmount[`${fromTo.from.name}`])
   }
+  const setAmountToHalf = () => {
+    //console.log("setting amount to max");
+    document.getElementById('swapInput').value = maxAmount[`${fromTo.from.name}`]/2
+    saveAmount(maxAmount[`${fromTo.from.name}`]/2)
+  }
+  const setAmountTo25 = () => {
+    //console.log("setting amount to max");
+    document.getElementById('swapInput').value = maxAmount[`${fromTo.from.name}`]/4
+    saveAmount(maxAmount[`${fromTo.from.name}`]/4)
+  }
 
   /* define max amount depending on pool/wallet balances when balances are updated */
   useEffect(() => {
-    //console.log(balance);
-    //console.log(currencies);
     if(balance[0]) {
-      if(currencies.from === 'ISC') {
-        let tempAmount = maxAmount
-        //console.log('swapping from ISC')
+      /* Swapping from SOLANA ISC */
+      if(fromTo.from.name === 'ISC') {
+        //let tempAmount = maxAmount
         const userbal = balance[1].ethereum
         const poolbal = balance[3].solana
         if (userbal >= poolbal) {
-          tempAmount.ISC = userbal;
-          setMaxAmount(tempAmount)
+          //tempAmount.ISC = userbal;
+          //setMaxAmount(tempAmount)
+          setMaxAmount((amounts) => ({...amounts, 'ISC':userbal}))
         }
         else {
-          tempAmount.ISC = poolbal;
-          setMaxAmount(tempAmount)
+          setMaxAmount((amounts) => ({...amounts, 'ISC':poolbal}))
+          //setMaxAmount(tempAmount)
         }
-        //console.log(userbal, poolbal);
       } 
-      else if (currencies.from === 'xOil') {
+      /* SWAPPING FROM XOIL TO ISC ON ETHEREUM */
+      else if (fromTo.from.name === 'xOil') {
         const userbal = balance[1].ethereum
-        let tempAmount = maxAmount
-        tempAmount.xOil = userbal;
-        setMaxAmount(tempAmount)
+        // let tempAmount = maxAmount
+        // tempAmount.xOil = userbal;
+        setMaxAmount((amounts) => ({...amounts, 'xOil':userbal}))
       }
+      /* SWAPPING ISC FROM ETHEREUM */
       else if (currencies.from === 'ethISC') {
 
       }
+      /* SWAPPING OIL TO ISC ON SOLANA */
       else if (currencies.from === 'Oil') {
 
       } 
       else {
-        console.log('unsupported currency');
+        //console.log('unsupported currency');
       }
     }
-  },[balance])
+  }, [balance])
 
   useEffect(()=>{},[maxAmount])
   return (
     <p className={styles.SwapMax}>
-      <button type='button' onClick={setAmountToMax}>Max</button>  
-      {maxAmount[`${fromTo.from.name}`]} ISC</p>
+      <button type='button' onClick={setAmountTo25}>25%</button> 
+      <button type='button' onClick={setAmountToHalf}>Half</button> 
+      <button type='button' onClick={setAmountToMax}>Max</button> 
+      <img src='./wallet.svg' height={15} className={styles.maxWalletIcon}/> 
+      {maxAmount[`${fromTo.from.name}`]} {fromTo.from.name}
+    </p>
   )
 }
