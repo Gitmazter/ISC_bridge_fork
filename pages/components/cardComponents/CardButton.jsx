c32fb947c00a58c74823c1a87faa786238f51384
gitmazter <philip.andelic@gmail.com>
2023-09-07 22:58:30 +0700

    implement waiting state and text animation

--- pages/components/cardComponents/CardButton.jsx
+++ pages/components/cardComponents/CardButton.jsx
@@ -1,9 +1,42 @@
+import { useEffect, useState } from 'react';
 import styles from '../../../styles/mystyle.module.css'
 
-export default function CardButton({value, enable, click_handler}) {
+export default function CardButton({value, enable, click_handler, waiting}) {
+  const [ display, setDisplay ] = useState(false);
+  const waitingMessageBase = 'Waiting for signature....'
+  const [ waitingMessage, setWaitingMessage ] = useState(waitingMessageBase)
+
+  useEffect(()=>{
+    let interval = undefined
+    let i = 21
+    if (waiting) {
+      interval = setInterval(()=> {
+        if (i < waitingMessageBase.length) {
+          setWaitingMessage(waitingMessageBase.substring(0,i))
+          i++
+        }
+        else {
+          i = 21;
+          setWaitingMessage(waitingMessageBase.substring(0,i))
+          i++;
+        }
+      }, 333);
+    }
+    else {
+      clearInterval(interval)
+    }
+  },[waiting])
+
+
   return (
-    <div className={styles.action}>
-      <a className={enable?styles.button:styles.noDisplay} onClick={click_handler} href="#"> {value} </a>
-    </div>
+    waiting 
+    ?
+      <div className={styles.action_waiting}>
+        <button type='button' className={waiting?styles.button:styles.noDisplay} href="#"> {waitingMessage} </button>
+      </div>
+    :
+      <div className={styles.action}>
+        <button type='button' className={enable?styles.button:styles.noDisplay} onClick={click_handler} href="#"> {value} </button>
+      </div>
   )
 }
