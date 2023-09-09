09619b999a893951d39bf67bd6047a9cc24f44f4
gitmazter <philip.andelic@gmail.com>
2023-09-09 16:12:40 +0700

    rm logs

--- pages/components/contexts/AmountContext.jsx
+++ pages/components/contexts/AmountContext.jsx
@@ -6,11 +6,11 @@ export const AmountContextProvider = ({children}) => {
   const [amount, setAmount] = useState(0.01); /* Default amount */
 
   const saveAmount = (val) => {
-    console.log("new value", val);
+    // console.log("new value", val);
     setAmount(val)
   }
 
-  useEffect(() => {console.log("amount updated");} , [amount])
+  // useEffect(() => {console.log("amount updated");} , [amount])
 
   return (
     <AmountContext.Provider value={{amount, saveAmount}}>
