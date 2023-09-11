786b65b56f2341474983d54f89cef0f6f97e14fa
gitmazter <philip.andelic@gmail.com>
2023-09-11 22:26:59 +0700

    add body style *Temp fix*

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google"
 import BridgeApp from "./v2Components/apps/App"
-
+import styles from '../styles/mystyle.module.css'
 const inter = Inter({
   subsets: ['latin'],
   weight: '700',
@@ -10,7 +10,9 @@ const App = () => {
   return(
     <main className={inter.className}>
       {/* <Header/> */}
-      <BridgeApp/>
+      <div className={styles.body}>
+        <BridgeApp/>
+      </div>
       {/* <Footer/> */}
     </main>
   )
