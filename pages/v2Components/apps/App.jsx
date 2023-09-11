4d9f37c11ed9ede1ebc281910b8152e90dddf16c
gitmazter <philip.andelic@gmail.com>
2023-09-11 22:27:31 +0700

    improved app

--- pages/v2Components/apps/App.jsx
+++ pages/v2Components/apps/App.jsx
@@ -1,19 +1,24 @@
+import { useState } from 'react'
 import styles from '../../../styles/mystyle.module.css'
 import { Card } from '../Card'
 
+
+
 const BridgeApp = () => {
+  const [currStep, setCurrStep] = useState(1)
   const steps = [1,2,3]
-  const html = steps.map((step) => {
-    return(
-      <Card step={step}/>
-    )
-  })
+
+  const html = steps.map(( step ) => {  return <Card step={step} currStep={currStep}/>  })
 
 
-  return (
+  return ( <>
+    {/* Temporary buttons */}
+            <button onClick={() => {currStep - 1 > 0 ? setCurrStep(currStep-1) : console.log()}}>Step Up</button>
+            <button onClick={() => {currStep + 1 < 4 ? setCurrStep(currStep+1) : console.log()}}>Step Down</button>
     <div className={styles.v2App}>
       {html}
     </div>
+    </>
   )
 }
 
