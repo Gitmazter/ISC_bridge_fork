31027743827e84d03181201d5fe73e500afca497
gitmazter <philip.andelic@gmail.com>
2023-09-11 22:57:13 +0700

    add const type = 'map'

--- pages/v2Components/cardComponents/mapComponents/MapTokenCard.jsx
+++ pages/v2Components/cardComponents/mapComponents/MapTokenCard.jsx
@@ -1,29 +1,28 @@
 import styles from '../../../../styles/mystyle.module.css'
 import {SolIcon, EthIcon, IscIcon, EthIscIcon} from '../tokenIcons/Icons'
 const MapTokenCard = ({index}) => {
-
+  const type = 'map'
   const CardTopics = [
     'Solana ISC',
     'Solana Bridge ISC',
     'Ethereum Bridge ISC',
     'Ethereum ISC',
-  ]
-
+  ];
   const cardIcons = () => {
     switch(index){
       case 1:
-        return <> <SolIcon type={'map'}/> <IscIcon type={'map'}/> </>
+        return <> <SolIcon type={type}/> <IscIcon type={type}/> </>
       case 2:
         return (
-          <> <SolIcon type={'map'}/> <EthIscIcon type={'map'}/> </>
+          <> <SolIcon type={type}/> <EthIscIcon type={type}/> </>
         )
       case 3:
         return (
-          <> <EthIcon type={'map'}/> <EthIscIcon type={'map'}/></>
+          <> <EthIcon type={type}/> <EthIscIcon type={type}/></>
         )
       case 4:
         return (
-          <> <EthIcon type={'map'}/> <EthIscIcon type={'map'}/></>
+          <> <EthIcon type={type}/> <EthIscIcon type={type}/></>
         )
     }
   }
