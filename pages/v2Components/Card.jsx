7da6d24a0138c5ce9444c2f3305d1f566e46e0cd
gitmazter <philip.andelic@gmail.com>
2023-09-11 22:27:13 +0700

    add steps to title and map cards

--- pages/v2Components/Card.jsx
+++ pages/v2Components/Card.jsx
@@ -3,12 +3,12 @@ import CardTitle from './cardComponents/CardTitle'
 import CardBody from './cardComponents/CardBody'
 import CardMap from './cardComponents/CardMap'
 
-export const Card = ({step}) => {
+export const Card = ({step, currStep}) => {
   return (
-    <div className={styles.v2Card}> {/* Frame */}
-      <CardTitle step={step}/> {/* Title with step and currStep ellipse*/}
-      <CardMap/> {/* Map with current */}
-      <CardBody/> {/* Body With Swap Card */}
+    <div className={styles.v2Card}>                {/* Frame */}
+      <CardTitle step={step} currStep={currStep}/> {/* Title with step and currStep ellipse*/}
+      <CardMap step={step}/>                       {/* Map with current */}
+      <CardBody/>                                  {/* Body With Swap Card */}
     </div>
   )
 }
