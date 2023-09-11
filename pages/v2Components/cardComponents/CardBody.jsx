63e462ae6c2537bf87cb30cfa75ed7bc3d771b67
gitmazter <philip.andelic@gmail.com>
2023-09-11 22:56:46 +0700

    Add components for body

--- pages/v2Components/cardComponents/CardBody.jsx
+++ pages/v2Components/cardComponents/CardBody.jsx
@@ -1,21 +1,29 @@
 import styles from '../../../styles/mystyle.module.css'
+import ActionButton from './bodyComponents/ActionButton'
+import ActiveInput from './bodyComponents/ActiveInput'
+import InactiveInput from './bodyComponents/InactiveInput'
 
 const CardBody = () => {
   // TODO
   // Max Amounts
   // Connection Check
-  // 
-
+  /* 
+  Implement: 
+    Balance Context
+    Bridge Scripts (Rewrite old app to class with exported fns?)
+    Max Amounts
+    Connectivity check (in order of need as in Figma)
+    
+  */
+  const prompt = 'swap'
 
   return (
     <div className={styles.CardBody}>
       <div className={styles.CardBodyInner}> 
-        <div>
-          <div>TOKEN 1</div>
-          <div>ARROW DOWN</div>
-          <div>TOKEN 2</div>
-        </div>
-        <div>BUTTON</div>
+        <ActiveInput/>
+        <div>ARROW</div>
+        <InactiveInput/>
+        <ActionButton prompt={prompt}/>
       </div>
     </div>
   )
