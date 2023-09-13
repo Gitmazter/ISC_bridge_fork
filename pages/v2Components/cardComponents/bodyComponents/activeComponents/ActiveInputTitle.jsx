2cbf9e72440ff3002d7ea3cb2787cfe7ec501f1d
gitmazter <philip.andelic@gmail.com>
2023-09-13 19:18:08 +0700

    display max amount

--- pages/v2Components/cardComponents/bodyComponents/activeComponents/ActiveInputTitle.jsx
+++ pages/v2Components/cardComponents/bodyComponents/activeComponents/ActiveInputTitle.jsx
@@ -4,14 +4,20 @@ import BodyConfig from '../config/BodyConfig'
 import { useContext, useEffect } from 'react'
 import { AmountContext } from '../../../contexts/amountContext'
 import { StepContext } from '../../../contexts/stepContext'
+import { BalanceContext } from '../../../contexts/balanceContext'
+import { MaxAmountContext } from '../../../contexts/maxAmountContext'
 
 const ActiveInputTitle = () => {
   const {step} = useContext(StepContext)
+  const { balance } = useContext(BalanceContext)
+  const { maxAmounts } = useContext(MaxAmountContext)
 
   const direction = 'solToEth'
   const prompt = () => {
     return BodyConfig[`${direction}`].activePrompts[step-1]
   }
+
+
   return (
     <div className={styles.inputTitle}>
       <div>{prompt()}</div>
@@ -19,7 +25,7 @@ const ActiveInputTitle = () => {
       {/* BreakOut */}
       <div className={styles.maxDisplay}>
         <WalletIcon/>
-        <div>Max Amount Source</div>
+        <div>{maxAmounts[step-1] + " " + BodyConfig[`${direction}`].tokenNames[step-1]}</div>
         <button>Max</button>
       </div>
 
