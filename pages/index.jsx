325f38debcb74eb363aff9ff0015e8c3e16f1495
gitmazter <philip.andelic@gmail.com>
2023-09-13 19:17:05 +0700

    add application context

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,15 +9,22 @@ import { SolanaContextProvider } from "./v2Components/app/web3/solConnect"
 import Header from './layout/Header'
 import { Web3Provider } from "@ethersproject/providers"
 import { Web3ReactProvider } from "@web3-react/core"
+import { ApplicationContext } from "./v2Components/contexts/applicationContext"
+import Loading from "./components/Loading"
 const inter = Inter({
   subsets: ['latin'],
   weight: '700',
 })
 
 const App = () => {
-  const [ maxAmounts, setMaxAmounts ] = useState();
+  const [ maxAmounts, setMaxAmounts ] = useState([<Loading/>, <Loading/>, <Loading/>, <Loading/>]);
   const [ balance, setBalance ] = useState();
   const [ direction, setDirection ] = useState('solToEth');
+  const [ application, setApplication] = useState()
+
+  const saveApplication = (app) => {
+    setApplication(app)
+  }
 
   const saveDirection = (dir) => {
     setDirection(dir)
@@ -41,12 +48,16 @@ const App = () => {
         <DirectionContext.Provider value={{direction, saveDirection}}>
           <BalanceContext.Provider value={{balance, saveBalance}}>
             <MaxAmountContext.Provider value = {{maxAmounts, saveMaxAmounts}}>
+              <ApplicationContext.Provider value={{application, saveApplication}}>
+                
               <main className={inter.className}>
                 <Header/>
                 <div className={styles.body}>
                   <BridgeApp/>
                 </div>
               </main>
+
+              </ApplicationContext.Provider>
             </MaxAmountContext.Provider>
           </BalanceContext.Provider>
         </DirectionContext.Provider>
