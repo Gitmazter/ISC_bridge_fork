6ece7d995a1c8d547b8cd4002be1c500bd18523e
gitmazter <philip.andelic@gmail.com>
2023-09-15 15:16:17 +0700

    new rpcs

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,6 +1,6 @@
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import styles from '../../styles/mystyle.module.css'
-import EthModalButton from '../../components/v2Components/app/web3/EthConnectButton';
+import EthModalButton from '../v2Components/app/web3/EthConnectButton';
 export default function Header () {
   return (
     <header className={styles.header}>
--- next.config.js
+++ next.config.js
@@ -2,24 +2,4 @@
 module.exports = {
   // Specify your desired base path here:
   basePath: '', // Replace with your desired base path
-
-  // Optional: Add any additional Next.js configurations here:
-  // For example, you can configure custom webpack behavior, plugins, etc.
-
-  // Example custom webpack configuration:
-  // webpack: (config, { isServer }) => {
-  //   // Add custom webpack rules or plugins here
-  //   // Example: Load SVG files using raw-loader
-  //   config.module.rules.push({
-  //     test: /\.svg$/,
-  //     use: ['raw-loader'],
-  //   });
-
-  //   return config;
-  // },
-
-  // Add more Next.js configurations as needed...
-
-  // Example: Use assetPrefix to specify CDN path for assets
-  // assetPrefix: 'https://cdn.example.com/',
 };
