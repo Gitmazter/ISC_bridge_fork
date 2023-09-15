5d884f86fbb6f34ee6a2dcbd851de7b42a2c3b7a
gitmazter <philip.andelic@gmail.com>
2023-09-15 18:30:08 +0700

    add outfiles

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
+
+
+
+
 // next.config.js
 module.exports = {
   // Specify your desired base path here:
   basePath: '', // Replace with your desired base path
+  output: 'export', /* export */
 };
