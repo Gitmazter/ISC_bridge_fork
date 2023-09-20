2368c00873f0d41963820ad0ab9199f62bc120d8
gitmazter <philip.andelic@gmail.com>
2023-09-20 19:57:12 +0700

    agaen

--- v2walletScripts/wallet-wormhole.mjs
+++ v2walletScripts/wallet-wormhole.mjs
@@ -271,9 +271,8 @@ class WalletWormhole {
             async (transaction) => {
                 console.log('signing tx');
                 transaction = await keypair.signTransaction(transaction)
-                transaction.recentBlockhash = (await this.connection.getLatestBlockhash()).blockhash;
-                transaction.lastValidBlockHeight =(await this.connection.getLatestBlockhash()).lastValidBlockHeight;
-                // transaction.partialSign(keypair.sign);
+                // transaction.recentBlockhash = (await this.connection.getLatestBlockhash()).blockhash;
+                // transaction.lastValidBlockHeight =(await this.connection.getLatestBlockhash()).lastValidBlockHeight;
                 console.log(transaction);
                 return transaction;
             },
