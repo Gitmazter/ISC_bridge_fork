4b1405078977b9350686e99cf3e8218300f641c1
gitmazter <philip.andelic@gmail.com>
2023-09-20 19:54:24 +0700

    agin

--- v2walletScripts/wallet-wormhole.mjs
+++ v2walletScripts/wallet-wormhole.mjs
@@ -262,7 +262,7 @@ class WalletWormhole {
     }
 
     async complete_transfer_on_solana(vaaBytes) {
-        const keypair = this.wallets.solana;
+        const keypair = this.wallets[0];
         console.log(keypair);
         console.log(this.connection);
         console.log(await this.connection.getLatestBlockhash());
