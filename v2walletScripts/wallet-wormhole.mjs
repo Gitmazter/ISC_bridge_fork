b47ea34c130b42d6cb2050eca9f60a19e71842eb
gitmazter <philip.andelic@gmail.com>
2023-09-22 00:13:09 +0700

    wait for confirmation

--- v2walletScripts/wallet-wormhole.mjs
+++ v2walletScripts/wallet-wormhole.mjs
@@ -28,7 +28,7 @@ class WalletWormhole {
         this.programId = new PublicKey(this.config.solana.swap_contract);
         this.isc = new PublicKey(this.config.solana.isc);
         this.oil = new PublicKey(this.config.solana.oil);
-        this.connection = new Connection(rpcConfig.solana.rpc, 'finalized')
+        this.connection = new Connection(rpcConfig.solana.rpc, 'confirmed')
 
         // this.connection._rpcWsEndpoint = config.solana.wss;
         this.options = {
