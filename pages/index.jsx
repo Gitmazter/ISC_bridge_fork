4b6a131e0cb8b8eb81f4ba83eb079e65466ffd53
gitmazter <philip.andelic@gmail.com>
2023-09-21 23:02:53 +0700

    add body height

--- pages/index.jsx
+++ pages/index.jsx
@@ -58,6 +58,7 @@ const App = () => {
                       overflow-x: hidden;
                       margin: 0px;
                       padding: 0px;
+                      height:100vh;
                         }
                       `}
                   </style>
