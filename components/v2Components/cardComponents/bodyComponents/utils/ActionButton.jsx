43fbc29e4426d2ca93dae930ee088f0cac0a4a3c
gitmazter <philip.andelic@gmail.com>
2023-09-23 18:34:50 +0700

    edit to confirmed

--- components/v2Components/cardComponents/bodyComponents/utils/ActionButton.jsx
+++ components/v2Components/cardComponents/bodyComponents/utils/ActionButton.jsx
@@ -29,7 +29,7 @@ const ActionButton = () => {
   const { connected, connect } = useWallet()
   const {connection} = useConnection()
   const { active, activate, library: provider} = useWeb3React()
-  const solConnection = new Connection(config.solana.rpc, "finalized")
+  const solConnection = new Connection(config.solana.rpc, "confirmed")
   // solConnection._rpcWsEndpoint = config.solana.wss;
   // // solConnection.underlyingSocket.url = config.solana.wss;
   const solSigner = useWallet();
