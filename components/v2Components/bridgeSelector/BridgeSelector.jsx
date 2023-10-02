b3aa5d46779648e2dd4143a43b6c239d467339d6
gitmazter <philip.andelic@gmail.com>
2023-10-02 17:56:14 +0700

    init bridge selector

---
+++ components/v2Components/bridgeSelector/BridgeSelector.jsx
@@ -0,0 +1,19 @@
+import styles from '../../../styles/mystyle.module.css';
+import { BridgeInput } from './BridgeInput';
+
+
+export const BridgeSelector = () => {
+
+  const chain1 = {name: "Solana", imgUrl: "./solana.svg"  };
+  const chain2 = {name: "Ethereum", imgUrl: "./ethereum.svg"  };
+
+
+  return (
+    <div className={styles.bridgeSelector}>
+      
+      <BridgeInput chaindata={chain1}/>
+      <img src='/new/ArrowInCircle.svg' className={styles.turn270}/>
+      <BridgeInput chaindata={chain2}/>
+    </div>
+  )
+}
