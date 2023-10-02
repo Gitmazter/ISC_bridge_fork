2e7f0d0f3317c05deb6a43962df61fbdedcfef6a
gitmazter <philip.andelic@gmail.com>
2023-10-02 17:55:05 +0700

    Add nav

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -2,28 +2,33 @@ import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import styles from '../../styles/mystyle.module.css'
 import EthModalButton from '../v2Components/app/web3/EthConnectButton';
 import { useEffect, useState } from 'react';
+import { Nav } from './headerComponents/nav';
 export default function Header () {
   const [clientRendered, setClientRendered] = useState()
 
-  useEffect(() => {
-    setClientRendered(
-      <>
-        <WalletMultiButton/>
-        <EthModalButton />
-      </>
-    )
-  }, [])
+  // useEffect(() => {
+  //   setClientRendered(
+  //     <>
+  //       <WalletMultiButton/>
+  //       <EthModalButton />
+  //     </>
+  //   )
+  // }, [])
 
 
   return (
     <header className={styles.header}>
       <div className={styles.headerInfo}> 
         <img src='./ISC-logo.svg' height={50} className={styles.logo}></img>
-        <h1>ISC BRIDGE</h1>
+        <div className={styles.brandText}>
+          <h1>ISC</h1>
+          <p>International Stable Currency</p>
+        </div>
       </div>
-      <div className={styles.walletContainer}>
-        {clientRendered}
-      </div>
+      <Nav/>
+      {/* <div className={styles.walletContainer}> */}
+        {/* {clientRendered} */}
+      {/* </div> */}
     </header>
   )
 }
